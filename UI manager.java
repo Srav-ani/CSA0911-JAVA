549a4c48f91ded66ea369309dee9ba12d1b6123d
Srav-ani <113418961+Srav-ani@users.noreply.github.com>
2023-10-23 09:17:48 +0530

    Create UI manager.java

---
+++ UI manager.java
@@ -0,0 +1,8 @@
+UI manager
+class UIManager {
+
+    static void setLookAndFeel(String comjtattooplafsmartSmartLookAndFeel) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
